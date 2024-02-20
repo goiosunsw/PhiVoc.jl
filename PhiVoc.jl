2ef60a9039e103847fca052143269ff4192925da
Andre Almeida <a.almeida@unsw.edu.au>
2024-02-21 09:37:12 +1100

    First commit

---
+++ PhiVoc.jl
@@ -0,0 +1,105 @@
+using FFTW, DSP
+
+
+function findPeaks(x)
+	pkmask = (x[1:end-2] .< x[2:end-1]) .& (x[3:end] .< x[2:end-1])
+	pkmask = [false; pkmask; false]
+end
+
+function highestNPeaks(x; n=5)
+	pmask = findPeaks(x)
+	pkIdx = findall(pmask)
+	n = min(n, length(pkIdx))
+	sortIdx = sortperm(x[pmask])
+	pkIdx[sortIdx[end:-1:end-n+1]]
+end
+
+function framePairsToPeakFreqs(framePair, lag::Integer, npk::Integer)
+	nfft = (size(framePair,1)-1)*2
+	# calculate average power spectrum for peak estimation
+	frav = sum((abs.(framePair)).^2, dims=2)
+	# select highest peaks
+	pki = highestNPeaks(frav, n=npk)
+	# phase differences between frames
+	dphi = diff(angle.(framePair[pki,:]),dims=2)
+	# peak bin centre frequencies
+	cf = (pki.-1)./(nfft-1)
+	# expected wrap factor for peak bins
+	nwrap = round.(cf.*lag)
+	# possible frequencies (different wrap factors possible in adjacent frames)
+	fopt = (dphi/2pi .+ nwrap .+ (-1:1)' )./lag
+	freqs = [fopt[ii, argmin(abs.(cf[ii].-fopt[ii,:]))] for ii in axes(fopt,1)]
+	ampls = frav[pki]
+	(freqs, ampls, pki)
+end
+	
+
+function pvocSingleFrame(x; nfft::Integer=1024, lag::Integer=256, window::Union{Function,Nothing}=nothing, npk::Integer=10)
+	# Nyquist frame (non-redundant spectrum)
+	inyq = nfft÷2+1
+	# pre-allocate frames
+	frames = zeros(ComplexF64,inyq,2)
+	if !isnothing(window)
+		win = window(nfft)
+	end
+	
+	# calculate 2 frames of spectrum
+	for (ii, ist) in enumerate([0,lag])
+		xfr = x[1+ist:nfft+ist]
+		if !isnothing(window)
+			xfr .*= win
+		end
+		Xfr = fft(xfr)
+		frames[:,ii] .= Xfr[1:inyq]
+	end
+	framePairsToPeakFreqs(frames, lag, npk)
+end
+
+function stft(x;  nfft::Integer=1024, lag::Integer=256, window::Union{Function,Nothing}=nothing)
+	nx = length(x)
+	if !isnothing(window)
+		win = window(nfft)
+	end
+	
+	fridx = 1:lag:(nx-nfft)
+	nfr = length(fridx)
+	nnyq = nfft ÷ 2 + 1
+	sx = zeros(Complex{eltype(x)}, nnyq, nfr)
+	for (ifr, ist) in enumerate(fridx)
+		xfr = x[1+ist:nfft+ist]
+		if !isnothing(window)
+			xfr .*= win
+		end
+		Xfr = fft(xfr)
+		sx[:,ifr] .= Xfr[1:nnyq]
+	end
+	if isnothing(window)
+		wsum = sqrt(nfft)
+	else
+		wsum = sqrt(sum(win.^2))
+	end
+	wsum *= sqrt(nfft/2)
+	sx./wsum
+end
+
+function pvocSignal(x; nfft::Integer=1024, lag::Integer=256, window::Union{Function,Nothing}=nothing, npk::Integer=10)
+	sx = stft(x, nfft=nfft, lag=lag, window=window)
+	nfr = size(sx,2)
+	freqs = zeros(nfr-1, npk)
+	pows = zeros(nfr-1, npk)
+	nnyq = size(sx,1)
+	for ii in 1:nfr-1
+		framePair = sx[:,ii:ii+1]
+		f, p, ib = framePairsToPeakFreqs(framePair, lag, npk)
+		nf = length(f)
+		freqs[ii, 1:nf] = f
+		#pows[ii, 1:nf] = p./2
+		# sum neighbouring bins to calculate partial power
+		for (fidx, binidx) in enumerate(ib)
+			minbin = max(binidx-3,1)
+			maxbin = min(binidx+3,nnyq)
+			pows[ii,fidx] = sum(abs.(framePair[minbin:maxbin,:]).^2)/2
+		end
+	end
+	(freqs, pows)
+end
---
+++ testPhiVoc.jl
@@ -0,0 +1,25 @@
+using Test, DSP
+include("PhiVoc.jl")
+
+x = ones(100)
+xpeaks = [1,10,20,30]
+x[xpeaks] .= [2,3,4,5]
+
+# test peak finder
+@test findall(findPeaks(x)) == [10,20,30]
+
+# test n peak finder
+@test highestNPeaks(x; n=2) == xpeaks[[end,end-1]]
+
+# test n peak finder when n>actual nbr of peaks
+@test highestNPeaks(x; n=8) == [30, 20, 10]
+
+t = (0.0:1.0:2048.0)
+y = sin.(2pi.*t./20)
+@test isapprox(pvocSingleFrame(y; nfft=1024, lag=256, window=hanning, npk=10)[1][1],0.05,rtol=1e-6)
+
+# test stft with defaults
+@test size(stft(y)) == (513, 5)
+
+# test stft amplitude
+@test isapprox(sum(abs.(stft(y)[:,2]).^2), .5, rtol=0.1)
